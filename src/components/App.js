07a8246befc019f7f2dd27abeb4f90da1d7b0353
Valeriia <valeriia.air@gmail.com>
2023-07-02 22:54:08 +0300

    say hi

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider } from 'styled-components';
-import { theme, light, dark } from '../theme';
+import { ThemeProvider } from "styled-components";
+import { theme, light, dark } from "../theme";
 
 import Header from "./Header/Header";
 import { HomePage } from "../pages/HomePage/HomePage";
 import { ContactsPage } from "../pages/ContactsPage/ContactsPage";
 import { DrinksPage } from "../pages/DrinksPage/DrinksPage";
-import {BeerPage} from '../pages/BeerPage/BeerPage'
-import {PromotionsPage} from '../pages/PromotionsPage/PromotionsPage'
-import {SnacksPage} from '../pages/SnacksPage/SnacksPage'
+import { BeerPage } from "../pages/BeerPage/BeerPage";
+import { PromotionsPage } from "../pages/PromotionsPage/PromotionsPage";
+import { SnacksPage } from "../pages/SnacksPage/SnacksPage";
 
 import NavigationMenu from "./NavigationMenu/NavigationMenu";
 import { BodyContainer } from "./App.styled";
@@ -16,21 +16,21 @@ import { BodyContainer } from "./App.styled";
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <BodyContainer>
+        <Header></Header>
+        <NavigationMenu></NavigationMenu>
 
-    <BodyContainer>
-      <Header></Header>
-      <NavigationMenu></NavigationMenu>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/drinks" element={<DrinksPage />} />
+          <Route path="/beer" element={<BeerPage />} />
+          <Route path="/snacks" element={<SnacksPage />} />
+          <Route path="/promotions" element={<PromotionsPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+        </Routes>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/drinks" element={<DrinksPage />} />
-        <Route path="/beer" element={<BeerPage />} />
-        <Route path="/snacks" element={<SnacksPage/>} />
-        <Route path="/promotions" element={<PromotionsPage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
-
-    </BodyContainer>
+        <div>Привітики від Андрія :)</div>
+      </BodyContainer>
     </ThemeProvider>
   );
 };
